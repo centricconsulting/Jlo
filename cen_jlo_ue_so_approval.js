eda8731df076b2cb5a40a2a9a8cac677e65d0fe7
AmanCentric <aman.bhadoriya@centricconsulting.com>
2023-07-21 21:18:51 +0530

    Final SO approval

--- cen_jlo_ue_so_approval.js
+++ cen_jlo_ue_so_approval.js
@@ -7,68 +7,90 @@
  * 1.0  July 14, 2023   Centric Consulting(Aman)     Initial Version
 \= = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = */
 
-define(['N/search', 'N/record'],
-    function (search, record) {
+define(['N/search', 'N/record', 'N/runtime'],
+    function (search, record, runtime) {
+        function beforeLoad(context) {
+            // get approve button
+            var mode = context.type;
+            if (mode == 'view') {
+                log.debug({ title: 'Mode', details: mode });
+                var objForm = context.form;
+                var billButton = objForm.getButton({ id: 'approve' });
+                if (billButton) {
+                    billButton.isHidden = true;
+                }
+            }
+        }
         function myAfterSubmit(context) {
             try {
-                var currentRecord = context.newRecord;
-                var soIntId = currentRecord.getValue({ fieldId: "id" });
-                var customer = currentRecord.getValue({ fieldId: "entity" });
-                log.debug({ title: 'so Detail', details: "int ID: " + soIntId + ", Cust: " + customer });
+                var executionContext = runtime.executionContext;
+                log.debug({ title: 'executionContext', details: executionContext });
+                if (executionContext == 'USERINTERFACE') {
+                    var mode = context.type;
+                    var currentRecord = context.newRecord;
+                    var customer = currentRecord.getValue({ fieldId: "entity" });
+                    if (mode == 'create' || mode == 'edit') {
+                        log.debug({ title: 'Mode', details: mode });
+                        var soIntId = currentRecord.getValue({ fieldId: "id" });
+                        var customer = currentRecord.getValue({ fieldId: "entity" });
+                        log.debug({ title: 'so Detail', details: "int ID: " + soIntId + ", Cust: " + customer });
 
-                // checking for B2B Customer
-                var fieldLookUp = search.lookupFields({
-                    type: search.Type.CUSTOMER,
-                    id: customer,
-                    columns: ['isperson']
-                });
-                var isPerson = fieldLookUp.isperson;
-                if (isPerson == false) {
-                    return;
-                }
-                var soRecord = record.load({
-                    type: record.Type.SALES_ORDER,
-                    id: soIntId,
-                    isDynamic: false
-                });
-                var itemCount = soRecord.getLineCount({ sublistId: 'item' });
-                log.debug({ title: 'Item Count', details: itemCount });
-                var flag;
-                for (var i = 0; i < itemCount; i++) {
-                    var SO_Item = soRecord.getSublistValue({ sublistId: 'item', fieldId: 'item', line: i });
-                    var SO_Item_type = soRecord.getSublistValue({ sublistId: 'item', fieldId: 'itemtype', line: i });
-                    var SO_Item_Quantity = soRecord.getSublistValue({ sublistId: 'item', fieldId: 'quantity', line: i });
+                        // checking for B2B Customer
+                        var fieldLookUp = search.lookupFields({
+                            type: search.Type.CUSTOMER,
+                            id: customer,
+                            columns: ['isperson']
+                        });
+                        var isPerson = fieldLookUp.isperson;
+                        if (isPerson == true) {
+                            return;
+                        }
+                        var soRecord = record.load({
+                            type: record.Type.SALES_ORDER,
+                            id: soIntId,
+                            isDynamic: false
+                        });
+                        var itemCount = soRecord.getLineCount({ sublistId: 'item' });
+                        log.debug({ title: 'Item Count', details: itemCount });
+                        var flag;
+                        for (var i = 0; i < itemCount; i++) {
+                            var SO_Item = soRecord.getSublistValue({ sublistId: 'item', fieldId: 'item', line: i });
+                            var SO_Item_type = soRecord.getSublistValue({ sublistId: 'item', fieldId: 'itemtype', line: i });
+                            var SO_Item_Quantity = soRecord.getSublistValue({ sublistId: 'item', fieldId: 'quantity', line: i });
 
-                    //check for avaiable quantity
-                    var availableCount;
-                    if (SO_Item_type == "Kit") {
-                        availableCount = checkAvailableKitItemCount(SO_Item);
+                            //check for avaiable quantity
+                            var availableCount;
+                            if (SO_Item_type == "Kit") {
+                                availableCount = checkAvailableKitItemCount(SO_Item);
+                            }
+                            if (SO_Item_type == "InvtPart") {
+                                availableCount = checkAvailableInvItemCount(SO_Item);
+                            }
+                            if (availableCount >= SO_Item_Quantity) {
+                                flag = true;
+                            } else {
+                                flag = false;
+                            }
+                            if (flag == false) { break; }
+                        }
+                        log.debug({ title: 'flag: ', details: flag });
+                        if (flag == false) {
+                            soRecord.setValue({ fieldId: 'orderstatus', value: "A" });
+                            soRecord.setValue({ fieldId: 'status', value: "Pending Approval" });
+                            soRecord.setValue({ fieldId: 'custbody_so_approval', value: true });
+                        }
+                        if (flag == true) {
+                            soRecord.setValue({ fieldId: 'orderstatus', value: "B" });
+                            soRecord.setValue({ fieldId: 'status', value: "Pending Fulfillment" });
+                        }
+                        soRecord.save({
+                            enableSourcing: true,
+                            ignoreMandatoryFields: true
+                        });
                     }
-                    if (SO_Item_type == "InvtPart") {
-                        availableCount = checkAvailableInvItemCount(SO_Item);
-                    }
-                    if (availableCount >= SO_Item_Quantity) {
-                        flag = true;
-                    } else {
-                        flag = false;
-                    }
-                    if (flag == false) { break; }
                 }
-                log.debug({ title: 'flag: ', details: flag });
-                if (flag == false) {
-                    soRecord.setValue({ fieldId: 'orderstatus', value: "A" });
-                    soRecord.setValue({ fieldId: 'status', value: "Pending Approval" });
-                }
-                if (flag == true) {
-                    soRecord.setValue({ fieldId: 'orderstatus', value: "B" });
-                    soRecord.setValue({ fieldId: 'status', value: "Pending Fulfillment" });
-                }
-                soRecord.save({
-                    enableSourcing: true,
-                    ignoreMandatoryFields: true
-                });
             } catch (e) {
-                log.debug({ title: 'Error on action: ', details: e });
+                log.debug({ title: 'Error on after Submit: ', details: e });
             }
         }
 
@@ -131,6 +153,7 @@ define(['N/search', 'N/record'],
             }
         }
         return {
+            beforeLoad: beforeLoad,
             afterSubmit: myAfterSubmit
         };
     });
