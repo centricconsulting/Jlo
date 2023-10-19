8ce8821b2f1a8960cb2c45641e3e86f079d12513
AmanCentric <aman.bhadoriya@centricconsulting.com>
2023-10-19 18:16:07 +0530

    script cust dep in invoice

--- cen_jlo_ue_update_order_id.js
+++ cen_jlo_ue_update_order_id.js
@@ -19,11 +19,26 @@ define(['N/record', 'N/search'], function (record, search) {
             var tranType = newRecord.type;
             // To update sales order
             if (tranType == "salesorder") {
-                var eTailOrderID = newRecord.getValue({ fieldId: 'custbody_celigo_etail_order_id' });
+                var eTailOrderID = newRecord.getValue({ fieldId: 'custbody_celigo_etail_order_id' }); // old field
+                var shofipyOrderID = newRecord.getValue({ fieldId: 'custbody_shopify_order_id' }); // new field
                 log.debug("Detail", "Type: " + tranType + ", ID: " + tranID + ", eTailOrderID: " + eTailOrderID)
                 var shopify_Original_Order_ID;
-                if (!eTailOrderID) {
-                    var SORecord = record.load({ type: record.Type.SALES_ORDER, id: tranID, isDynamic: true, });
+                if (!shofipyOrderID && eTailOrderID) {
+                    record.submitFields({
+                        type: record.Type.SALES_ORDER,
+                        id: 1,
+                        values: {
+                            'custbody_shopify_order_id': eTailOrderID
+                        },
+                        options: {
+                            enableSourcing: false,
+                            ignoreMandatoryFields: true
+                        }
+                    });
+                }
+
+                if (!shofipyOrderID && !eTailOrderID) {
+                    var SORecord = record.load({ type: record.Type.SALES_ORDER, id: tranID, isDynamic: true });
                     var itemCount = SORecord.getLineCount({ sublistId: 'item' });
                     for (var i = 0; i < itemCount; i++) {
                         shopify_Original_Order_ID = SORecord.getSublistValue({
@@ -36,29 +51,30 @@ define(['N/record', 'N/search'], function (record, search) {
                         }
                     }
                     if (shopify_Original_Order_ID) {
-                        SORecord.setValue({ fieldId: 'custbody_celigo_etail_order_id', value: shopify_Original_Order_ID });
+                        SORecord.setValue({ fieldId: 'custbody_shopify_order_id', value: shopify_Original_Order_ID });
                     }
                     SORecord.save({ enableSourcing: true, ignoreMandatoryFields: true });
                 }
             }
             // To update Invoice
             if (tranType == "invoice") {
-                var eTailOrderID = newRecord.getValue({ fieldId: 'custbody_celigo_etail_order_id' });
+                var eTailOrderID = newRecord.getValue({ fieldId: 'custbody_celigo_etail_order_id' }); // old field
+                var shofipyOrderID = newRecord.getValue({ fieldId: 'custbody_shopify_order_id' }); // new field
                 log.debug("Detail", "Type: " + tranType + ", ID: " + tranID + ", eTailOrderID: " + eTailOrderID)
                 var shopify_Original_Order_ID;
-                if (!eTailOrderID) {
-                    var invRecord = record.load({ type: record.Type.INVOICE, id: tranID, isDynamic: true, });
+                if (!shofipyOrderID) {
+                    var invRecord = record.load({ type: record.Type.INVOICE, id: tranID, isDynamic: true });
                     var createdForm = invRecord.getValue({ fieldId: 'createdfrom' });
                     if (createdForm) {
                         var soFlds = search.lookupFields({
                             type: record.Type.SALES_ORDER,
                             id: createdForm,
-                            columns: ["custbody_celigo_etail_order_id"],
+                            columns: ["custbody_shopify_order_id"],
                         });
-                        shopify_Original_Order_ID = soFlds.custbody_celigo_etail_order_id;
+                        shopify_Original_Order_ID = soFlds.custbody_shopify_order_id;
                     }
                     if (shopify_Original_Order_ID) {
-                        invRecord.setValue({ fieldId: 'custbody_celigo_etail_order_id', value: shopify_Original_Order_ID });
+                        invRecord.setValue({ fieldId: 'custbody_shopify_order_id', value: shopify_Original_Order_ID });
                     }
                     invRecord.save({ enableSourcing: true, ignoreMandatoryFields: true });
                 }
