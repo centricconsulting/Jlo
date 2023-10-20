7afa04a863b705a6854b9f2461a08e50231743ba
rmazuk <ron.mazuk@centricconsulting.com>
2023-10-20 11:14:22 -0400

    Changes to SO approval script. It was not complete, but it looks like we will no longer need the script.

--- cen_jlo_ue_so_approval.js
+++ cen_jlo_ue_so_approval.js
@@ -5,15 +5,27 @@
  * Purpose:  Send email for Effort Supervisor Group when record status update to open/close.
  * VER  DATE            AUTHOR               	 	 CHANGES
  * 1.0  July 14, 2023   Centric Consulting(Aman)     Initial Version
+ * For B2B users, force approval
+ * - B2B is checked based on the customer attribute
+	
+ * For B2C users:
+ * 	- if any line is a subscription and does not have a original price, then force an approval
+ * 	- (remove) if there is not enough quantity for any line, then force approval
+ *  - (remove) For B2C users if there is enough quantity for all lines, auto approve
+ * 
+ * Modify to check count by location: https://suiteanswers.custhelp.com/app/answers/detail/a_id/74934/loc/en_US
+ * Kit components: https://suiteanswers.custhelp.com/app/answers/detail/a_id/46201/loc/en_US
 \= = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = */
 
 define(['N/search', 'N/record', 'N/runtime'],
     function (search, record, runtime) {
         function beforeLoad(context) {
+
+            // Mazuk - what does this do?
             // get approve button
             var mode = context.type;
-            if (mode == 'view') {
-                log.debug({ title: 'Mode', details: mode });
+            //log.debug({ title: 'Mode', details: mode });
+            if (mode == 'view') {                
                 var objForm = context.form;
                 var billButton = objForm.getButton({ id: 'approve' });
                 if (billButton) {
@@ -25,97 +37,108 @@ define(['N/search', 'N/record', 'N/runtime'],
             try {
                 var executionContext = runtime.executionContext;
                 log.debug({ title: 'executionContext', details: executionContext });
-                if (executionContext == 'USERINTERFACE') {
-                    var mode = context.type;
-                    var currentRecord = context.newRecord;
-                    var customer = currentRecord.getValue({ fieldId: "entity" });
-                    if (mode == 'create') {
-                        log.debug({ title: 'Mode', details: mode });
-                        var soIntId = currentRecord.getValue({ fieldId: "id" });
-                        var customer = currentRecord.getValue({ fieldId: "entity" });
-                        log.debug({ title: 'so Detail', details: "int ID: " + soIntId + ", Cust: " + customer });
 
-                        // checking for B2B Customer
-                        var fieldLookUp = search.lookupFields({
-                            type: search.Type.CUSTOMER,
-                            id: customer,
-                            columns: ['isperson']
-                        });
-                        var isPerson = fieldLookUp.isperson;
-                        var soRecord = record.load({
-                            type: record.Type.SALES_ORDER,
-                            id: soIntId,
-                            isDynamic: true
-                        });
+                // Mazuk - removed the context because we need to handle CSV and WebServices contexts - to 
+                // cover any B2B csv loads and for any Amazon or Shopify orders
+                //if (executionContext == 'USERINTERFACE') {
+                var mode = context.type;
+                var currentRecord = context.newRecord;
+                if (mode == 'create') {
+                    log.debug({ title: 'Mode', details: mode });
+                    var soIntId = currentRecord.getValue({ fieldId: "id" });
+                    var customer = currentRecord.getValue({ fieldId: "entity" });
+                    log.debug({ title: 'so Detail', details: "int ID: " + soIntId + ", Cust: " + customer });
+
+                    // load the SO record.
+                    var soRecord = record.load({
+                        type: record.Type.SALES_ORDER,
+                        id: soIntId,
+                        isDynamic: true
+                    });
+
+                    // checking for B2B Customer
+                    var customer = currentRecord.getValue({ fieldId: "entity" });
+                    var fieldLookUp = search.lookupFields({
+                        type: search.Type.CUSTOMER,
+                        id: customer,
+                        columns: ['isperson']
+                    });
+                    var isPerson = fieldLookUp.isperson;
+
+                    // for B2B customers, we want to force an approval
+                    if (!isPerson) {
+                        log.debug('B2B Order', flag);
+                        soRecord.setValue({ fieldId: 'orderstatus', value: "A" });
+                        soRecord.setValue({ fieldId: 'status', value: "Pending Approval" });
+                        soRecord.setValue({ fieldId: 'custbody_so_approval', value: true });  
+                    }
+                    else // handle B2C customers
+                    {
+
+                        // loop through the item list, checking for the following:
+                        // 1) If the quantity requested is more than the available quantity, force an approval
+                        // 2) if the line item is a subscription, but there is no Shopify Original Proce, force an approval
                         var itemCount = soRecord.getLineCount({ sublistId: 'item' });
                         log.debug({ title: 'Item Count', details: itemCount });
-                        var flag;
-                        for (var i = 0; i < itemCount; i++) {
+                        var flag = false;
+                        for (var i = 0; i < itemCount && !flag; i++) {
                             var SO_Item = soRecord.getSublistValue({ sublistId: 'item', fieldId: 'item', line: i });
                             var SO_Item_type = soRecord.getSublistValue({ sublistId: 'item', fieldId: 'itemtype', line: i });
                             var SO_Item_Quantity = soRecord.getSublistValue({ sublistId: 'item', fieldId: 'quantity', line: i });
                             var SO_Item_Sopify_Org_prize = soRecord.getSublistValue({ sublistId: 'item', fieldId: 'custcol_shpfy_orgnl_prc', line: i });
-
+                            var SO_Item_Shopify_Sub = soRecord.getSublistValue({ sublistId: 'item', fieldId: 'custcol_shpfy_subscrptn_flg', line: i });
 
                             log.debug('SO_Item_Sopify_Org_prize', SO_Item_Sopify_Org_prize);
 
                             //check for avaiable quantity
+                            log.debug("type",SO_Item_type);
                             var availableCount;
                             if (SO_Item_type == "Kit") {
                                 availableCount = checkAvailableKitItemCount(SO_Item);
-                            }
-                            if (SO_Item_type == "InvtPart") {
+                            } else if (SO_Item_type == "InvtPart" || SO_Item_type == "Assembly") {
                                 availableCount = checkAvailableInvItemCount(SO_Item);
                             }
-                            if (availableCount >= SO_Item_Quantity) {
+                            else {
+                                log.debug("invalid type",SO_Item_type);
+                                availableCount = 0;
+                            }
+                            log.debug("quantities:avail:item",availableCount+":"+SO_Item_Quantity);
+                            if (availableCount < SO_Item_Quantity) {
                                 flag = true;
-                                log.debug('title1', flag);
-                            } else {
-                                flag = false;
-                                log.debug('title2', flag);
+                                log.debug('not enough quantity', flag);
                             }
-                            //        if (flag == false) { break; }
 
-                            log.debug({ title: 'flag: ', details: flag });
-                            if (flag == false && isPerson == false) {
-                                //      if (SO_Item_Sopify_Org_prize == null ) {
-                                //          soRecord.setValue({ fieldId: 'status', value: "Pending Approval" });
-
-                                log.debug('title3', flag);
-                                soRecord.setValue({ fieldId: 'orderstatus', value: "A" });
-                                soRecord.setValue({ fieldId: 'status', value: "Pending Approval" });
-                                soRecord.setValue({ fieldId: 'custbody_so_approval', value: true });
-                                //      }  
+                            // if this is a subscription item, and no original price was supplied, force an approval
+                            if (SO_Item_Shopify_Sub && SO_Item_Shopify_Sub == 'Y' && SO_Item_Sopify_Org_prize == '') {
+                                flag = true;
+                                log.debug('no shopify price', flag);
                             }
-                            else
-                                if ((flag == true && isPerson == true)) {
-                                    if (SO_Item_Sopify_Org_prize == '') {
-                                        soRecord.setValue({ fieldId: 'orderstatus', value: "A" });
-                                        soRecord.setValue({ fieldId: 'status', value: "Pending Approval" });
-                                        soRecord.setValue({ fieldId: 'custbody_so_approval', value: true });
-                                        soRecord.setValue({ fieldId: 'memo', value: 'TEST222' });
-                                        var custbody_so_approval1 = soRecord.getValue({ fieldId: 'custbody_so_approval' });
-                                        log.debug('custbody_so_approval', custbody_so_approval1);
-                                        soRecord.setValue({ fieldId: 'custbody_so_approval', value: true });
-                                        log.debug('title4', flag);
-                                        log.debug('SO_Item_Sopify_Org_prize new 0', SO_Item_Sopify_Org_prize);
-                                        break;
-                                    }
-                                    else {
-                                        soRecord.setValue({ fieldId: 'orderstatus', value: "B" });
-                                        soRecord.setValue({ fieldId: 'status', value: "Pending Fulfillment" });
-                                        log.debug('SO_Item_Sopify_Org_prize new 1', SO_Item_Sopify_Org_prize);
-                                    }
+                        } // end loop 
 
-                                }
-
+                        log.debug({ title: 'flag: ', details: flag });
+                        if ((flag == true)) {
+                            soRecord.setValue({ fieldId: 'orderstatus', value: "A" });
+                            soRecord.setValue({ fieldId: 'status', value: "Pending Approval" });
+                            soRecord.setValue({ fieldId: 'custbody_so_approval', value: true });
+                            ///soRecord.setValue({ fieldId: 'memo', value: 'TEST222' });
+                            //soRecord.setValue({ fieldId: 'custbody_so_approval', value: true });
+                            log.debug('mark for pending approval - B2C', flag);
+                            //log.debug('SO_Item_Sopify_Org_prize new 0', SO_Item_Sopify_Org_prize);
                         }
-                    }
-                }
-                soRecord.save({
-                    enableSourcing: true,
-                    ignoreMandatoryFields: true
-                });
+                        else {
+                            log.debug("mark approved");
+                            soRecord.setValue({ fieldId: 'orderstatus', value: "B" });
+                            soRecord.setValue({ fieldId: 'status', value: "Pending Fulfillment" });
+                            //log.debug('SO_Item_Sopify_Org_prize new 1', SO_Item_Sopify_Org_prize);
+                        }
+                    } // end B2B check
+
+                    // save the changes to the SO record
+                    soRecord.save({
+                        enableSourcing: true,
+                        ignoreMandatoryFields: true
+                    });
+                } // end Create Mode Check
             } catch (e) {
                 log.debug({ title: 'Error on after Submit: ', details: e });
             }
