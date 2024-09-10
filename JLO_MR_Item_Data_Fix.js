dda0feab5d56f6c299c934407c89f789590dfd15
rmazuk <ron.mazuk@centricconsulting.com>
2024-09-10 16:40:16 -0400

    Fixes to item fix script

--- JLO_MR_Item_Data_Fix.js
+++ JLO_MR_Item_Data_Fix.js
@@ -22,7 +22,7 @@ define(['N/record', 'N/search', 'N/runtime'], function (record, search, runtime)
                     "AND",
                     ["item", "anyof", originalItem],
                     "AND",
-                    ["internalidnumber", "equalto", "2160221"], //Remove once done testing
+                    ["internalidnumber", "equalto", "2160326"], //Remove once done testing
                     "AND",
                     ["item", "noneof", replacementItem],
                     "AND",
@@ -87,11 +87,14 @@ define(['N/record', 'N/search', 'N/runtime'], function (record, search, runtime)
             //Delete Related Payment Record
             deletePaymentApplication(recordId);
 
+            log.debug("delete deposit application");
+
             // Delete the invoice record
             record.delete({
                 type: record.Type.INVOICE,
                 id: recordId
             });
+            
             log.debug('Deleted Invoice', 'Invoice ID: ' + recordId + ' has been deleted.');
 
             if (createdFromId) {
@@ -170,22 +173,26 @@ define(['N/record', 'N/search', 'N/runtime'], function (record, search, runtime)
             start: 0,
             end: 1
         });
+        creditMemoResults.forEach(function (creditMemo) {
+            log.debug("credit memo",creditMemo.getValue({ name: 'internalid' }));
+        });       
+        log.debug("credit memo number",creditMemoResults.length);
 
         return creditMemoResults.length > 0;
     }
 
     /**
- * Delete the payment application related to an invoice
- * @param {string} invoiceId - ID of the invoice for which to delete the payment application
- */
+    * Delete the payment application related to an invoice
+    * @param {string} invoiceId - ID of the invoice for which to delete the payment application
+    */
     function deletePaymentApplication(invoiceId) {
         var paymentAppSearch = search.create({
-            type: "customerpayment",
+            type: search.Type.DEPOSIT_APPLICATION,
             settings: [{ "name": "consolidationtype", "value": "ACCTTYPE" }, { "name": "includeperiodendtransactions", "value": "F" }],
             filters:
                 [
-                    ["type", "anyof", "CustPymt"],
-                    "AND",
+                    //["type", "anyof", "CustPymt"],
+                    //"AND",
                     ["appliedtotransaction", "anyof", invoiceId]
                 ],
             columns:
@@ -201,8 +208,9 @@ define(['N/record', 'N/search', 'N/runtime'], function (record, search, runtime)
 
         paymentAppResults.forEach(function (paymentApp) {
             try {
+                log.debug("try deposit app",paymentApp.getValue({ name: 'internalid' }));
                 record.delete({
-                    type: record.Type.CUSTOMER_PAYMENT,
+                    type: record.Type.DEPOSIT_APPLICATION,
                     id: paymentApp.getValue({ name: 'internalid' })
                 });
                 log.debug('Deleted Payment Application', 'Payment ID: ' + paymentApp.getValue({ name: 'internalid' }));
